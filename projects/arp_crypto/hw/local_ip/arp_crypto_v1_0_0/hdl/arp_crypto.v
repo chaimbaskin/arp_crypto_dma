30df9328f65f5e402c69416553845c4967e8b7a4
yuryAud <yury.audzevich@gmail.com>
2015-08-06 23:50:12 +0300

    line 381 bug fix

--- projects/arp_crypto/hw/local_ip/arp_crypto_v1_0_0/hdl/arp_crypto.v
+++ projects/arp_crypto/hw/local_ip/arp_crypto_v1_0_0/hdl/arp_crypto.v
@@ -318,7 +318,7 @@ module arp_crypto
                 begin                                   
                     fifo_rd_en = 1;  
                     m_axis_tlast = 0;
-                    m_axis_tdata[255*8-1 : 22*8]    = 0;
+                    m_axis_tdata[32*8-1 : 22*8]    = 0;
                     m_axis_tdata[22*8-1  : 10*8]    = {size[31:24],size[23:16],size[15:8],size[7:0],offset[31:24],offset[23:16],offset[15:8],offset[7:0],MAGIC[31:24],MAGIC[23:16],MAGIC[15:8],MAGIC[7:0]}; 
                     m_axis_tdata[10*8-1  : 0]       = fifo_out_tdata;
                     m_axis_tkeep[31:0]              = {32{1'b1}};
